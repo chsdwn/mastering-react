1e8e0daeff455fd66ba68508df745cea27fc4865
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-01 18:57:07 +0300

    5-Embedding Expressions

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
   render() {
     return (
       // Works like ng-container
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
