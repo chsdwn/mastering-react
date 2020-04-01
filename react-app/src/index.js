c444ed7e941225aed7352317e9dee00220c8da65
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-01 17:49:22 +0300

    2-let vs var vs const

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,5 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// var => function
+// let => block
+// const => block
 
-const element = <h1>Hello World</h1>;
-ReactDOM.render(element, document.getElementById("root"));
+function sayHello() {
+  for (let i = 0; i < 5; i++) {
+    console.log("hello i");
+  }
+
+  for (var j = 0; j < 5; j++) {
+    console.log("hello j");
+  }
+
+  console.log(i);
+  console.log(j);
+}
+
+sayHello();
