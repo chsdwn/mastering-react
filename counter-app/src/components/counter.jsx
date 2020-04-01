6ab07e81d8f8c81e9f0c01c42bc40ade815e7799
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-01 18:52:07 +0300

    4-Specifying Children

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      // Works like ng-container
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
   }
 }
 
