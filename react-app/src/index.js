311aa54385dfb59cf68f7f341ff12891828021b1
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-01 18:30:51 +0300

    13-Modules

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,23 +1,4 @@
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
-
-  walk() {
-    console.log("walk");
-  }
-}
-
-class Teacher extends Person {
-  constructor(name, degree) {
-    super(name);
-    this.degree = degree;
-  }
-
-  teach() {
-    console.log("teach");
-  }
-}
+import Teacher from "./teacher";
 
 const teacher = new Teacher("Ali", "PhD");
 console.log(teacher.name);
---
+++ react-app/src/person.js
@@ -0,0 +1,9 @@
+export default class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  walk() {
+    console.log("walk");
+  }
+}
