948f7aede6f29fef081c341fc1040f3e2c7ecf28
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-01 18:08:55 +0300

    7-Arrow funtions and this

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,22 +1,16 @@
-const square = function(number) {
-  return number * number;
+const person = {
+  talk() {
+    var self = this;
+    setTimeout(function() {
+      console.log("this", self);
+    }, 1000);
+  },
+  talkAF() {
+    setTimeout(() => {
+      console.log("this", this);
+    }, 1000);
+  }
 };
 
-const squareAF = number => number * number;
-
-console.log(squareAF(3));
-
-const jobs = [
-  { id: 1, isActive: true },
-  { id: 2, isActive: true },
-  { id: 3, isActive: false }
-];
-
-const activeJobs = jobs.filter(function(job) {
-  return job.isActive;
-});
-
-const deactiveJobs = jobs.filter(job => !job.isActive);
-
-console.log(activeJobs);
-console.log(deactiveJobs);
+person.talk();
+person.talkAF();
