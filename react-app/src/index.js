7728288a6f7b5bbdebe12527395a5bd78f2bada7
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-01 18:13:11 +0300

    8-Array.map Method

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,16 +1,9 @@
-const person = {
-  talk() {
-    var self = this;
-    setTimeout(function() {
-      console.log("this", self);
-    }, 1000);
-  },
-  talkAF() {
-    setTimeout(() => {
-      console.log("this", this);
-    }, 1000);
-  }
-};
+const colors = ["red", "green", "blue"];
 
-person.talk();
-person.talkAF();
+const items = colors.map(function(color) {
+  return "<li>" + color + "</li>";
+});
+
+const itemsAF = colors.map(color => `<li>${color}</li>`);
+console.log(items);
+console.log(itemsAF);
