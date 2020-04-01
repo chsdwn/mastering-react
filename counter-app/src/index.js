ef51b0ae252d06414e0d294f989d8484de57f88d
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-01 18:47:53 +0300

    3-Your First React Component

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+import App from "./App";
+import Counter from "./components/counter";
+
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById("root")
 );
