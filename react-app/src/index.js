1afa4d04941f5b5bbd73ac3578619e66e1ffa92a
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-01 18:04:19 +0300

    6-Arrow functions

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,22 @@
-const person = {
-  name: "Ali",
-  walk() {
-    console.log(this);
-  }
+const square = function(number) {
+  return number * number;
 };
 
-person.walk(); // person
+const squareAF = number => number * number;
 
-const walk = person.walk.bind(person);
-walk(); // person
+console.log(squareAF(3));
+
+const jobs = [
+  { id: 1, isActive: true },
+  { id: 2, isActive: true },
+  { id: 3, isActive: false }
+];
+
+const activeJobs = jobs.filter(function(job) {
+  return job.isActive;
+});
+
+const deactiveJobs = jobs.filter(job => !job.isActive);
+
+console.log(activeJobs);
+console.log(deactiveJobs);
