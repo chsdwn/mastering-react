be9009674ea622fb77a22f3f434b591362cdc68b
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-01 18:23:56 +0300

    11-Classes

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,18 +1,13 @@
-const first = [1, 2, 3];
-const second = [4, 5, 6];
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
 
-const combined = first.concat(second);
-console.log(combined);
+  walk() {
+    console.log("walk");
+  }
+}
 
-const combinedES6 = [...first, 3.5, ...second];
-console.log(combinedES6);
-
-const clone = [...first];
-console.log(first);
-console.log(clone);
-
-const person1 = { name: "Ali" };
-const person2 = { job: "Carpenter" };
-
-const person = { ...person1, ...person2, location: "TR" };
-console.log(person);
+const person = new Person("Ali");
+console.log(person.name);
+person.walk();
