7cba374447736f135d3742f438a559da280a2b49
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-02 12:44:50 +0300

    3-Pagination Component Interface

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4
   };
 
   onMovieDeleteHandler = id => {
@@ -23,6 +25,8 @@ class Movies extends Component {
       movies
     });
   };
+
+  handlePageChange = page => {};
 
   render() {
     const { movies } = this.state;
@@ -72,6 +76,11 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={length}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
