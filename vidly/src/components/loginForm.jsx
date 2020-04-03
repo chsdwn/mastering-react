927332c6f88b23321f53bbf30ea1a947f90189f4
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-03 12:41:05 +0300

    3-Handling Form Submission

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log("submitted");
+  };
+
   render() {
     return (
       <div>
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input id="username" type="text" className="form-control" />
@@ -13,7 +19,9 @@ class LoginForm extends Component {
             <label htmlFor="password">Password</label>
             <input id="password" type="password" className="form-control" />
           </div>
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary" type="submit">
+            Login
+          </button>
         </form>
       </div>
     );
