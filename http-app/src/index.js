59ac3b66b30098b9dfa78ebf418fe8cd8b05dcaf
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-04 19:39:21 +0300

    15-Logging Errors

--- http-app/src/index.js
+++ http-app/src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import * as Sentry from "@sentry/browser";
 import registerServiceWorker from "./registerServiceWorker";
+
+import App from "./App";
+
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+
+Sentry.init({
+  dsn: "https://f567533f1f3e47979a701c86645e86ab@sentry.io/5188800",
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
