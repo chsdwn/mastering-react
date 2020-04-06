d5287990b03b2744eda9ae6abf29aa862b1faeb3
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-06 17:19:32 +0300

    10-Fetching Data with Hooks

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import Movie from "./hoc/Movie";
 import Counter from "./hooks/Counter";
+import Users from "./hooks/Users";
 
 import "./App.css";
 
 function App() {
-  return <Counter />;
+  return <Users />;
 }
 
 export default App;
